965e33ba7c34326c74c85aa69ee0aa6c194c1e23
4qWyn <goldenfreddi322@gmail.com>
2026-02-21 09:54:56 +0700

    Add files via upload

---
+++ IntegrationTesting/CalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("098d0f28-45a5-4a76-a915-99b92804fa71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IntegrationTesting/IntegrationTesting/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTesting
+{
+    public class Calculator
+    {
+        private float a, b;
+
+        public Calculator(float initA, float initB)
+        {
+            this.a = initA;
+            this.b = initB;
+        }
+
+        public double Calculate()
+        {
+            return a - Math.Pow(b, 2);
+        }
+
+        public void SetA(float value)
+        {
+            a = value;
+        }
+
+        public void SetB(float value)
+        {
+            b = value;
+        }
+
+        public float GetA()
+        {
+            return a;
+        }
+
+        public float GetB()
+        {
+            return b;
+        }
+    }
+}
